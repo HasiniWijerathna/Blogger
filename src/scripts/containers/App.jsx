f187a23c4709f22d6a9fb38d60799739a761aef5
HasiniWijerathna <hasini@calcey.com>
2017-02-16 11:45:09 +0530

    Style changes

--- src/scripts/containers/App.jsx
+++ src/scripts/containers/App.jsx
@@ -3,13 +3,13 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import HeaderBar from '../components/HeaderBar';
 import Footer from '../components/Footer';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import {grey700} from 'material-ui/styles/colors';
+import {grey900, grey700} from 'material-ui/styles/colors';
 
 const muiTheme = getMuiTheme({
   fontFamily: 'Overpass, sans-serif',
   palette: {
     primary1Color: grey700,
-    textColor: grey700,
+    textColor: grey900,
   },
 });
 
